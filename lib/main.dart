85237cf043710e29f2ee213f37629139c9d014fb
Jabsina P A <jabsinapa@gmail.com>
2025-08-07 01:24:48 +0530

    mainpage first background video set

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mars_app/feedback.dart';
+import 'package:mars_app/mars_home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home:FeedbackPage(),
+      home:WeatherControl(),
     );
   }
 }
--- lib/mars_home.dart
+++ lib/mars_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:video_player/video_player.dart';
 
 class WeatherControl extends StatefulWidget {
   const WeatherControl({super.key});
@@ -9,18 +10,79 @@ class WeatherControl extends StatefulWidget {
 }
 
 class _WeatherControlState extends State<WeatherControl> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset("assets/mars_weather_1.mp4")
+      ..initialize().then((_) {
+        _controller.setLooping(true);
+        _controller.setVolume(0.0); // mute
+        _controller.play();
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          "weather",
-          style: GoogleFonts.orbitron(fontSize: 26,color: Colors.deepOrange),
-
-        ),
-      ),
       backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          // Background video
+          _controller.value.isInitialized
+              ? SizedBox.expand(
+            child: FittedBox(
+              fit: BoxFit.cover,
+              child: SizedBox(
+                width: _controller.value.size.width,
+                height: _controller.value.size.height,
+                child: VideoPlayer(_controller),
+              ),
+            ),
+          )
+              : const Center(child: CircularProgressIndicator()),
+
+          // Foreground content
+          SafeArea(
+            child: Column(
+              children: [
+                AppBar(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  title: Text(
+                    "Weather",
+                    style: GoogleFonts.orbitron(
+                      fontSize: 26,
+                      color: Colors.deepOrange,
+                    ),
+                  ),
+                  centerTitle: true,
+                ),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    "Mars Weather Simulation 🌌",
+                    style: GoogleFonts.orbitron(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
